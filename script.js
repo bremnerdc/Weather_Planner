7567feabe5bc2443979127e36408263fa3167565
bremnerdc <bremnerdc@gmail.com>
2020-04-17 16:08:40 -0700

    made minor changes to js file

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $("#search").on("click", function(){
-   var cityName = $("#search").val(); 
+   var cityName = $("#input").val(); 
 
-  $("#search").val("");
+  $("#input").val("");
 
   currentOWA(cityName);
   });
@@ -14,7 +14,7 @@ $(document).ready(function() {
   //append the list of cities
   function cityHistory(text) {
     var app = $("<li>").addClass("list-group list-group-flush").text(text);
-    $(".cityList").append(app);
+    $(".cityList").prepend(app);
   }
 
 
@@ -30,7 +30,7 @@ function currentOWA(cityName) {
 //create link for every search
 if (cities.indexOf(cityName) === -1) {
   cities.push(cityName);
-  window.localStorage.setItem("cityList", JSON.stringify(cityList));
+  window.localStorage.setItem("cityList", JSON.stringify(cities));
   cityHistory(cityName);
 }
    $("#today").empty();
@@ -47,10 +47,14 @@ if (cities.indexOf(cityName) === -1) {
   var windspd = $("<p>").addClass("card-text").text("Wind Speed: " + response.wind.speed);
   var body = $("#today");
   title1.append(img);
-  body.append(title, temp, hum, windspd);
+  body.append(title1, temp, hum, windspd);
   //call other 2 ajax calls//function
+
+  lat = response.coord.lat;
+  lon = response.coord.lon;
   forecastOWA(cityName);
-  uviOWA(response.coord.lat, response.coord.lon);
+  uviOWA(lat, lon);
+
     });
   }
 // $(api.openweathermap.org/data/2.5/forecast?q={city name}&appid={6da06b04e4767b3319293983361bca86}
@@ -64,30 +68,55 @@ if (cities.indexOf(cityName) === -1) {
       method: "GET",
      dataType: 'json',
     }).then(function(response) {
-      addTableRow(response);
-    });
-  }
+     
 
-  for (var i = 0; i < response.list.length; i++) {
-    if (response.list[i].dt_txt.indexOf("15:00:00") !== -1){
-  //parts from project go here
-  var forecastDiv = $("<div>").addClass("col-md-3");
-  var card = $("<div>").addClass("card bg-primary text-white ");
-  var body = $("<div>").addClass("card-body");
-  var img = $("<img>").attr("src", "http://openweathermap.org/img/w" + response.list[i].weather[0].icon + ".png");
-  var title = $("<h5>").addClass("card-title").text(new Date(response.list[i].dt_txt).toLocaleDateString());
-  var p1 = $("<p>").addClass("card-text").text("Temp: " + response.list[i].main.temp);
-  var p2 = $("<p>").addClass("card-text").text("Humidity: " + response.list[i].main.humidity);
 
-  forecastDiv.append(card.append(body.append(title, p1, p2, img, body)));
-              $("#forecast.row").append;
+//   for (var i = 0; i < response.list.length; i++) {
+//     if (response.list[i].dt_txt.indexOf("15:00:00") !== -1){
+//   //parts from project go here
+//   var forecastDiv = $("<div>").addClass("col-md-3");
+//   var card = $("<div>").addClass("card bg-primary text-white ");
+//   var body = $("<div>").addClass("card-body");
+//   var img = $("<img>").attr("src", "http://openweathermap.org/img/w" + response.list[i].weather[0].icon + ".png");
+//   var title = $("<h5>").addClass("card-title").text(new Date(response.list[i].dt_txt).toLocaleDateString());
+//   var p1 = $("<p>").addClass("card-text").text("Temp: " + response.list[i].main.temp);
+//   var p2 = $("<p>").addClass("card-text").text("Humidity: " + response.list[i].main.humidity);
 
+//   forecastDiv.append(card.append(body.append(title, p1, p2, img, body)));
+//               $("#forecast .row").append(forecastDiv);
+
+// }
+//   }
+// Creating weather report div elements
+var weatherReport = $("<div class='card blue-grey'>");
+var cityNameEl = $("<span class='card-title'>" + location + " -  " + currentDate + "</span>");
+var weatherIconEl = $("<img src=http://openweathermap.org/img/w/" + weatherIcon + ".png" + ">");
+var descriptionEl = $("<p>  " + description + "</p>");
+var tempEl = $("<p>Temperature: " + tempF + "°F" + "</p><br>");
+var humidityEl = $("<p> Humidity: " + humidity + "%" + "</p><br>");
+var windSpeedEl = $("<p> Wind Speed: " + windSpeed + " m/s" + "</p><br>");
+var uvIndexEl = $("<p> UV Index: </p>");
+var cardContentDiv = $("<div class='card-content white-text'>");
+
+// Appending them to the weather report div space
+weatherReport.append(cardContentDiv);
+cardContentDiv.append(cityNameEl);
+cardContentDiv.append(descriptionEl);
+cardContentDiv.append(weatherIconEl);
+cardContentDiv.append(tempEl);
+cardContentDiv.append(humidityEl);
+cardContentDiv.append(windSpeedEl);
+uvIndexEl.append(uvIndex);
+cardContentDiv.append(uvIndexEl);
+$("#forecast").append(weatherReport);
+});
+
+// });
 }
-  }
-  //uv index
-function uviOWA() {
+//uv index http://api.openweathermap.org/data/2.5/uvi?q=&appid=6da06b04e4767b3319293983361bca86=47.61&lon=47.61
+function uviOWA(lat, lon) {
   var query3URL =
-  "http://api.openweathermap.org/data/2.5/uvi?q=&appid=6da06b04e4767b3319293983361bca86" + lat + "&lon=" + lon;
+  "http://api.openweathermap.org/data/2.5/uvi?q=&appid=6da06b04e4767b3319293983361bca86&lat=" + lat + "&lon=" + lon;
   $.ajax({
     url: query3URL,
     method: "GET",
@@ -96,9 +125,9 @@ function uviOWA() {
     var uvi = $('<p>').text("UV Index: ");
     var button = $('<span>').addClass("btn btn-sm").text(response.value);
     //color changes on UV value
-    if (data.value < 3){
+    if (response.value < 2){
       button.addClass("btn-success");
-     } else if (data.value < 6){
+     } else if (response.value >= 3 && response.value <= 7.99){
         button.addClass("btn-warning");
       }
       else {
@@ -112,6 +141,12 @@ function uviOWA() {
   
   
 
-var cities = JSON.parse(windowLocalStorage.getItem(".cityList"));
+var cities = JSON.parse(window.localStorage.getItem(".cityList")) || [];
 
+if (cities.length > 0) {
+  currentOWA(cities[cities.length-1]);
+}
+for (var i = 0; i < cities.length; i++) {
+  cityHistory(cities[i])
+}
   });
