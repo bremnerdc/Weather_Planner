c028219bc938e9924cd9fd130cc4a9c36ade23fa
bremnerdc <bremnerdc@gmail.com>
2020-04-21 22:44:04 -0700

    add https to api calls

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 
 function currentOWA(cityName) {
     var query1URL =
-    "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=6da06b04e4767b3319293983361bca86&units=imperial";
+    "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=6da06b04e4767b3319293983361bca86&units=imperial";
     $.ajax({
       url: query1URL,
       method: "GET",
@@ -40,7 +40,7 @@ if (cities.indexOf(cityName) === -1) {
    //jhtml content for daily weather
   // var card1 = $("<div>").addClass("card bg-primary text-white ");
   // var body = $("<div>").addClass("card-body");
-  var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+  var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
   var title1 = $("<h5>").addClass("card-title").text(response.name +" (" + new Date().toLocaleDateString() + ")");
   var temp = $("<p>").addClass("card-text").text("Temp: " + response.main.temp);
   var hum = $("<p>").addClass("card-text").text("Humidity: " + response.main.humidity);
@@ -62,7 +62,7 @@ if (cities.indexOf(cityName) === -1) {
   //forecast 
   function forecastOWA(cityName) {
     var query2URL =
-    "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=6da06b04e4767b3319293983361bca86&units=imperial";
+    "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=6da06b04e4767b3319293983361bca86&units=imperial";
     $.ajax({
       url: query2URL,
       method: "GET",
@@ -79,7 +79,7 @@ if (cities.indexOf(cityName) === -1) {
   var forecastDiv = $("<div>").addClass("col-md-2");
   var card = $("<div>").addClass("card bg-primary text-white ");
   var body = $("<div>").addClass("card-body");
-  var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png");
+  var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png");
   var title = $("<h5>").addClass("card-title").text(new Date(response.list[i].dt_txt).toLocaleDateString());
   var p1 = $("<p>").addClass("card-text").text("Temp: " + response.list[i].main.temp);
   console.log(response.list[i].main.temp);
@@ -95,7 +95,7 @@ if (cities.indexOf(cityName) === -1) {
 //uv index http://api.openweathermap.org/data/2.5/uvi?q=&appid=6da06b04e4767b3319293983361bca86=47.61&lon=47.61
 function uviOWA(lat, lon) {
   var query3URL =
-  "http://api.openweathermap.org/data/2.5/uvi?q=&appid=6da06b04e4767b3319293983361bca86&lat=" + lat + "&lon=" + lon;
+  "https://api.openweathermap.org/data/2.5/uvi?q=&appid=6da06b04e4767b3319293983361bca86&lat=" + lat + "&lon=" + lon;
   $.ajax({
     url: query3URL,
     method: "GET",
